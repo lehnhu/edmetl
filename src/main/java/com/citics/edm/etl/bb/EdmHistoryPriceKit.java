22827c3ed127f9bb383cbe2485d3fd256d215796
lehnhu <huliang87@gmail.com>
2014-05-09 18:14:36 +0800

    MOD:临时

--- src/main/java/com/citics/edm/etl/bb/EdmHistoryPriceKit.java
+++ src/main/java/com/citics/edm/etl/bb/EdmHistoryPriceKit.java
@@ -2,8 +2,12 @@ package com.citics.edm.etl.bb;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -41,10 +45,7 @@ public class EdmHistoryPriceKit {
 	@Autowired
 	@Qualifier("gsJdbcTemplate")
 	private JdbcTemplate gsJdbcTemplate;
-	//
-	// @Autowired
-	// private ArgHelper argHelper;
-	//
+
 	@Autowired
 	private IEDMPropertyService EDMPropertyService;
 
@@ -57,6 +58,7 @@ public class EdmHistoryPriceKit {
 			.getStringProperty("edmetl.bb.ispc_mapping");
 		Gson gson = new Gson();
 		ispc_mapping = gson.fromJson(json_str, Map.class);
+
 	}
 
 	/**
@@ -97,50 +99,132 @@ public class EdmHistoryPriceKit {
 		}
 	}
 
-	protected void produceBatchs(List<BBHistoryPriceItem> bblist,
-		boolean alwaysInsert) throws SQLException, IllegalArgumentException,
-		IllegalAccessException {
-		if (alwaysInsert) {
-			Field[] _fs = BBHistoryPriceItem.class.getDeclaredFields();
-			for (Field _f : _fs) {
-				if (_f.isAnnotationPresent(EDMField.class)) {
-					EDMField fieldAttr = _f.getAnnotation(EDMField.class);
-					String fieldName = fieldAttr.value().equalsIgnoreCase("") ? _f
-						.getName() : fieldAttr.value();
-					Class<?> fieldType = fieldAttr.type();
-					final Map<String, String> values = ispc_mapping
-						.get(fieldName);
-					if (values == null) {
-						LOG.warn("cannot find mapping for " + values);
-					} else {
-						final List<BBHistoryPriceItem> actualSaveList = new ArrayList<BBHistoryPriceItem>();
-						for (BBHistoryPriceItem bb : bblist) {
-							if (bb.getLinkageIssue() != null) {
-								actualSaveList.add(bb);
-							}
+	private void singleInsertSinglePriceField(final Field f,
+		List<BBHistoryPriceItem> bblist) {
+
+		final List<BBHistoryPriceItem> validPriceValueList = new ArrayList<BBHistoryPriceItem>();
+		for (BBHistoryPriceItem bb : bblist) {
+			try {
+				String priceValue = (String) f.get(bb);
+				priceValue = priceValue == null ? priceValue : priceValue
+					.toString();
+				if (priceValue == null || priceValue.equals("")
+					|| priceValue.equals("N.A.")) {
+					continue;
+				}
+				validPriceValueList.add(bb);
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+		if (validPriceValueList.size() == 0) {
+			return;
+		}
+
+		final SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMdd");
+		EDMField fieldAttr = f.getAnnotation(EDMField.class);
+		String fieldName = fieldAttr.value().equalsIgnoreCase("") ? f.getName()
+			: fieldAttr.value();
+		final String[] argTypes = insert_ispc_query.argTypes();
+		final String[] argNames = insert_ispc_query.argNames();
+		final String[] argValues = new String[argTypes.length];
+		Map<String, String> values = ispc_mapping.get(fieldName);
+		for (int i = 0; i < argTypes.length; i++) {
+			argValues[i] = values.get(argNames[i]);
+		}
+		BatchPreparedStatementSetter setter = new BatchPreparedStatementSetter() {
+			@Override
+			public void setValues(PreparedStatement pstmt, int row)
+				throws SQLException {
+				BBHistoryPriceItem bb = validPriceValueList.get(row);
+				for (int i = 0; i < argTypes.length; i++) {
+					if ("prc_tms".equalsIgnoreCase(argNames[i])) {
+						try {
+							pstmt.setDate(i + 1,
+								new java.sql.Date(fmt.parse(bb.getDate())
+									.getTime()));
+						} catch (ParseException e) {
+							e.printStackTrace();
 						}
-						if (actualSaveList.size() > 0) {
-							BatchPreparedStatementSetter setter = new BatchPreparedStatementSetter() {
-
-								@Override
-								public void setValues(PreparedStatement pstmt,
-									int i) throws SQLException {
-									BBHistoryPriceItem bb = actualSaveList
-										.get(i);
-									for (String key : values.keySet()) {
-
-									}
-								}
-
-								@Override
-								public int getBatchSize() {
-									return actualSaveList.size();
-								}
-							};
+					} else if ("unit_cprc".equalsIgnoreCase(argNames[i])) {
+						try {
+							pstmt.setBigDecimal(i+1, new BigDecimal((String)f.get(bb)));
+						} catch (IllegalArgumentException e) {
+							e.printStackTrace();
+						} catch (IllegalAccessException e) {
+					
+						}
+					}else{
+						if("VARCHAR".equalsIgnoreCase(argTypes[i])){
+							pstmt.setString(i+1, argValues[i]);
+						}else if("NUMBER".equalsIgnoreCase(argTypes[i])){
+							pstmt.setBigDecimal(i+1, new BigDecimal(argValues[i]));
+						}else if("DATE".equalsIgnoreCase(argTypes[i])){
+							try {
+								pstmt.setDate(i + 1,
+									new java.sql.Date(fmt.parse(argValues[i]).getTime()));
+							} catch (ParseException e) {
+								e.printStackTrace();
+							}
+						}else{
+							
 						}
 					}
 				}
 			}
+
+			@Override
+			public int getBatchSize() {
+				return validPriceValueList.size();
+			}
+		};
+
+		int[] rs = gsJdbcTemplate
+			.batchUpdate(insert_ispc_query.query(), setter);
+		System.out.println(rs);
+	}
+
+	private boolean check(Field _f) {
+		if (_f.getDeclaringClass() != String.class
+			|| !_f.isAnnotationPresent(EDMField.class)) {
+			return false;
+		}
+		EDMField fieldAttr = _f.getAnnotation(EDMField.class);
+		String fieldName = fieldAttr.value().equalsIgnoreCase("") ? _f
+			.getName() : fieldAttr.value();
+		Map<String, String> values = ispc_mapping.get(fieldName);
+		if (values == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	private void batchInsertOnly(List<BBHistoryPriceItem> bblist) {
+		final List<BBHistoryPriceItem> actualSaveList = new ArrayList<BBHistoryPriceItem>();
+		for (BBHistoryPriceItem bb : bblist) {
+			if (bb.getLinkageIssue() != null) {
+				actualSaveList.add(bb);
+			}
+		}
+		if (actualSaveList.size() > 0) {
+			Field[] _fs = BBHistoryPriceItem.class.getDeclaredFields();
+			for (Field _f : _fs) {
+				if (check(_f)) {
+					_f.setAccessible(true);
+					singleInsertSinglePriceField(_f, actualSaveList);
+				}
+			}
+		}
+	}
+
+	protected void loadBatchs(List<BBHistoryPriceItem> bblist,
+		boolean alwaysInsert) throws SQLException, IllegalArgumentException,
+		IllegalAccessException {
+		if (alwaysInsert) {
+			batchInsertOnly(bblist);
 		} else {
 
 		}
